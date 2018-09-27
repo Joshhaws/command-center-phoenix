840daac4f1ac43e4259ae159d7ed753c09e8f0c2
Josh Haws <joshhaws0@gmail.com>
2018-09-26 23:47:53 -0500

    migrations and pulling from database

--- lib/commandcenterphoenix/accounts/accounts.ex
+++ lib/commandcenterphoenix/accounts/accounts.ex
@@ -2,24 +2,26 @@ defmodule Commandcenterphoenix.Accounts do
     @moduledoc """
     The Accounts context
     """
-
     alias Commandcenterphoenix.Accounts.User
-
-    def list_users do
-        [
-            %User{id: "1", name: "Josh", username: "joshhaws"},
-            %User{id: "2", name: "Hayley", username: "hayleyhaws"},
-            %User{id: "2", name: "Oliver", username: "oliverhaws"}
-        ]
-    end
+    alias Commandcenterphoenix.Repo
 
     def get_user(id) do
-        Enum.find(list_users(), fn map -> map.id == id end)
+       Repo.get(User, id)
+    end
+
+    def get_user!(id) do
+        Repo.get!(User, id)
     end
 
     def get_user_by(params) do
-        Enum.find(list_users(), fn map ->
-            Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-        end)
+        Repo.get_by(User, params)
+    end
+
+    def list_users do
+        Repo.all(User)
+    end
+
+    def change_user(%User{} = user) do
+        User.changeset(user, %{})
     end
 end
--- lib/commandcenterphoenix_web/controllers/user_controller.ex
+++ lib/commandcenterphoenix_web/controllers/user_controller.ex
@@ -1,7 +1,8 @@
 defmodule CommandcenterphoenixWeb.UserController do
     use CommandcenterphoenixWeb, :controller
-
+    
     alias Commandcenterphoenix.Accounts
+
 
     def index(conn, _params) do
         users = Accounts.list_users()
@@ -11,5 +12,10 @@ defmodule CommandcenterphoenixWeb.UserController do
     def show(conn, %{"id" => id}) do
         user = Accounts.get_user(id)
         render(conn, "show.html", user: user)
+    end
+
+    def new(conn, _params) do
+        changeset = Accounts.change_user(%User{})
+        render(conn, "new.html", changeset: changeset)
     end
 end
